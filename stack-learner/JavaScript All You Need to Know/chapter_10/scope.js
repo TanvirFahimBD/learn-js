fac83400b25103d83cd3b145378e39691182e6ff
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-12-19 23:08:41 +0600

    scope concept clear

--- stack-learner/JavaScript All You Need to Know/chapter_10/scope.js
+++ stack-learner/JavaScript All You Need to Know/chapter_10/scope.js
@@ -14,3 +14,33 @@ function test() {
 }
 
 test()
+
+// lexical scope - small part token set each variable scope which means from where it can access when it goes to compiler or engine
+
+var a = 11
+
+function A() {
+    var b = 12
+    function B() {
+        var c = 23
+        console.log(c)
+    }
+
+    function C() {
+        var d = 56
+        console.log(d);
+    }
+    console.log(b)
+    D()
+    B()
+    C()
+}
+
+function D(n) {
+    return a + n
+}
+
+//A() -> a, b, B(), C(), D()
+//B() -> a, b, c, C(), D()
+//C() -> a, b, d, C(), D()
+//D() -> a, n , A()

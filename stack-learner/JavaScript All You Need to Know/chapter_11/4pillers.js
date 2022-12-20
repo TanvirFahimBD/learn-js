b77ddfae009f87c970d49d3df24dd81d66c9f3f5
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-12-20 14:24:12 +0600

    chapter 11 done

--- stack-learner/JavaScript All You Need to Know/chapter_11/4pillers.js
+++ stack-learner/JavaScript All You Need to Know/chapter_11/4pillers.js
@@ -3,4 +3,4 @@
 // 1. Abstraction - Hide unnecessary details from user
 // 2. Encapsulation - Binding/Capsulate properties or method
 // 3. Inheritance - Get properties or methods from others class
-// 4. Polymorphism - Different face of a single object
+// 4. Polymorphism - Different look of a single object

6d5104cc2ca21eded1c21a8e296bdb9b1c6492e9
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-12-20 12:19:22 +0600

    oop start

--- stack-learner/JavaScript All You Need to Know/chapter_10/iife.js
+++ stack-learner/JavaScript All You Need to Know/chapter_10/iife.js
@@ -1,6 +1,10 @@
 //096. Closure and Loops in Javascript
+//IIFE - immediately invoking function expression
+
 for (var index = 1; index <= 5; index++) {
-    setTimeout(function () {
-        console.log(index)
-    }, 1000 * index)
+    (function (n) {
+        setTimeout(function () {
+            console.log(n)
+        }, 1000 * n)
+    })(index)
 }
